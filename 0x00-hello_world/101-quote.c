dbb5adc28cc716021ce4d766f50045e32c2b4e09
Note <you@example.com>
2023-07-13 12:26:53 -0700

    Adv 2

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h> 
+#include <sys/syscall.h> 
+/**
+ * main - Entry point
+ *
+ * Return: always 1
+ */
+int main(void)
+{
+	const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"; 
+        syscall(SYS_write, 1, msg, sizeof(msg));
+	return (1);
+}
