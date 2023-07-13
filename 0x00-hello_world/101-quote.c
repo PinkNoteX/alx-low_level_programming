e9d01bacaa44772a5b85dfedc2d5b928d70f6ce2
Note <you@example.com>
2023-07-13 12:31:48 -0700

    Adv 2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
-#include <unistd.h> 
-#include <sys/syscall.h> 
+#include <unistd.h>
+#include <sys/syscall.h>
 /**
  * main - Entry point
  *
  * Return: always 1
  */
-int main(void)
+int
+main (void)
 {
-	const char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"; 
-        syscall(SYS_write, 1, msg, sizeof(msg));
-	return (1);
+  const char msg[] =
+    "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+  syscall (SYS_write, 1, msg, sizeof (msg));
+  return (1);
 }
