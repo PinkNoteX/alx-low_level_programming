4a10aa7d40cc36bd447d6fbe2e06bb3f4e8b284a
Note <you@example.com>
2023-07-14 14:08:42 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	x = 'Z';
+
+	while (x >= 'Z')
+	{
+		char small = tolower(x);
+
+		putchar(small);
+		x--;
+	}
+	putchar('\n');
+
+	return (0);
+}
