1ec418a1ae6f52131280c62bf6fcfaeb5f539933
Note <you@example.com>
2023-07-18 13:21:02 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+#include <ctype.h>
+/**
+ * print_alphabet - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int x;
+	int y = 11;
+
+	x = 'A';
+	while (y != 11) 
+	{
+	while (x <= 'Z')
+	{
+		char small = tolower(x);
+
+		_putchar(small);
+		x++;
+	}
+	_putchar('\n');
+	y--;
+	}
+}
