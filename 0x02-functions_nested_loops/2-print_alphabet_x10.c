39f6ddaebf6ee7378afa6ff741b747bd53b3cedc
Note <you@example.com>
2023-07-18 13:23:23 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,17 +3,17 @@
 #include "main.h"
 #include <ctype.h>
 /**
- * print_alphabet - Prints the alphabet in lowercase.
+ * print_alphabet_x10 - Prints the alphabet in lowercase.
  *
  * Return: Always 0 (Success)
  */
 void print_alphabet_x10(void)
 {
 	int x;
-	int y = 11;
+	int y = 10;
 
 	x = 'A';
-	while (y != 11) 
+	while (y != 0)
 	{
 	while (x <= 'Z')
 	{
