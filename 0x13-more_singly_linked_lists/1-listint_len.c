7762e5bfa3090394bd31a02ac9894d2e78fa14d7
PinkNoteX <omarfarghaly8@gmail.com>
2023-08-28 18:37:58 -0700

    Task 1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -6,13 +6,13 @@
 */
 size_t listint_len(const listint_t *h)
 {
-        size_t sum = 0;
+	size_t sum = 0;
+	
+	while (h != NULL)
+	{
+		sum++;
+		h = h->next;
+	}
 
-        while (h != NULL)
-        {
-                sum++;
-                h = h->next;
-        }
-
-        return (sum);
+	return (sum);
 }
