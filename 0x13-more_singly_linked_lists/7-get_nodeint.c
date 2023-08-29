4b9e208be119528a72998ef10f9ad06344a7596a
PinkNoteX <omarfarghaly8@gmail.com>
2023-08-28 19:19:11 -0700

    Task 7

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,23 @@
+#include "list.h"
+/**
+ * get_nodeint_at_index - gets node at index
+ * @head: linked list at head
+ * @index: index node to grab
+ * Return: node at index
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int sum = 0;
+	listint_t *tmp;
+
+	tmp = head;
+	while (count != index && tmp != NULL)
+	{
+		sum++;
+		tmp = tmp->next;
+	}
+
+	if (sum != index)
+		return (NULL);
+	return (tmp);
+}
