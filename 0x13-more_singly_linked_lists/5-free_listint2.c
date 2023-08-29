c7935c27d54a8b1f5c33eb6996a8ca38806d1b20
PinkNoteX <omarfarghaly8@gmail.com>
2023-08-28 18:59:10 -0700

    Task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_listint2 - free list while head is null
+ * @head: head
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	if (head == NULL)
+		return;
+	while (*head != NULL)
+	{
+		tmp = *head;
+		free(*head);
+		*head = tmp->next;
+	}
+	*head = NULL;
+}
