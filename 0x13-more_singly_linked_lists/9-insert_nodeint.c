137e7a1ea9514731de1480d477d517f33b80fb1f
PinkNoteX <omarfarghaly8@gmail.com>
2023-08-28 19:37:50 -0700

    Task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -21,7 +21,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		sum++;
 	}
 
-	if (count != idx - 1 && idx != 0)
+	if (sum != idx - 1 && idx != 0)
 		return (NULL);
 
 	new = malloc(sizeof(listint_t));
