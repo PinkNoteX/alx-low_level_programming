a2b36480b3b92e97a1dbe364a30452d199e4c80e
PinkNoteX <omarfarghaly8@gmail.com>
2023-08-28 19:23:34 -0700

    Task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,7 +11,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *tmp;
 
 	tmp = head;
-	while (sum < index && tmp != NULL)
+	while (sum != index && tmp != NULL)
 	{
 		sum++;
 		tmp = tmp->next;
