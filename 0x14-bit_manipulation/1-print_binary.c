02a70a44230be90735b38c464c6ab2168679bb64
PinkNoteX <omarfarghaly8@gmail.com>
2023-08-31 12:34:38 -0700

    Task 1 fixed

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,20 +10,20 @@
 
 void print_binary(unsigned long int n)
 {
-	int i, counter = 0;
-	unsigned long int currnum;
 
-	for (i = 63; i >= 0; i--)
+	if (n > 1)
 	{
-		currnum = n >> i;
-		if (currnum & 1)
-		{
+	print_binary(n >> 1);
+	}
+
+	if (n & 1)
+	{
 		_putchar('1');
-		counter++;
-		}
-		else if (counter)
+	}
+	else
+	{
 		_putchar('0');
 	}
-	if (!counter)
-		_putchar('0');
+
+
 }
