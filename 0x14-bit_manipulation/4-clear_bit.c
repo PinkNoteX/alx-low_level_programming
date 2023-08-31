ca90b70ae61d5928525c7c70cc7cc70443595925
PinkNoteX <omarfarghaly8@gmail.com>
2023-08-31 13:27:48 -0700

    Task 4 Fix

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,11 +10,13 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index < sizeof(unsigned long int) * 8)
+	if (index < 63)
 	{
-		*n &= (~(1 << index));
+		*n = (~(1UL << index) & *n);
 		return (1);
 	}
-
+	else
+	{
 	return (-1);
+	}
 }
