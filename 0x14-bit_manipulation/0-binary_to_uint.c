f2b3af046050171d3f49bcbc3c2e5023a40d1439
PinkNoteX <omarfarghaly8@gmail.com>
2023-08-31 10:10:46 -0700

    Task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,6 +13,9 @@ unsigned int binary_to_uint(const char *b)
 	int val = 1;
 	unsigned int total = 0;
 
+	if (!b)
+		return (0);
+
 	for (i = 0; b[i] != '\0'; i++)
 		;
 	for (i--; i >= 0; i--, val *= 2)
