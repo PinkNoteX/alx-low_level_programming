838fef2fec6b7ca5bceaa94fe4282a53214ac4e8
PinkNoteX <omarfarghaly8@gmail.com>
2023-09-04 19:14:11 -0700

    Adv Finished

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -184,6 +184,39 @@ void type_pr(unsigned char *e_ident)
 	}
 }
 /**
+ * entry_pr - Prints entry point
+ * @e_entry: address of entry point
+ * @e_ident: pointer to elf array
+ */
+void entry_pr(unsigned long int e_entry, unsigned char *e_ident)
+{
+	printf("  Entry point address:               ");
+
+	if (e_ident[EI_DATA] == ELFDATA2MSB)
+	{
+		e_entry = ((e_entry << 8) & 0xFF00FF00) | ((e_entry >> 8) & 0xFF00FF);
+		e_entry = (e_entry << 16) | (e_entry >> 16);
+	}
+	if (e_ident[EI_CLASS] == ELFCLASS32)
+		printf("%#x\n", (unsigned int)e_entry);
+	else
+		printf("%#lx\n", e_entry);
+}
+/**
+ * close_elf - closes elf file.
+ * @elf: descriptor
+ *
+ * Description: error 98 if file can't be closed
+ */
+void close_elf(int elf)
+{
+	if (close(elf) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", elf);
+		exit(98);
+	}
+}
+/**
 * main - Displays ELF Header
 * @c: number of args.
 * @v: pointer to args.
