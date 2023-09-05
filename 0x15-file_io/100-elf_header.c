2596631fd5a4eb2d82ed1143e278d286127a10a3
PinkNoteX <omarfarghaly8@gmail.com>
2023-09-04 18:17:24 -0700

    Adv Main finished and elf checker

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,1 +1,73 @@
-Adv1
+#include "main.h"
+
+/**
+* elf_checker - check for elf.
+* @e_ident: points to elf array.
+*
+* Description: checks the file to see if it's elf or not
+*/
+void elf_checker(unsigned char *e_ident)
+{
+	int ind;
+
+	for (ind = 0; ind < 4; ind++)
+	{
+		if (e_ident[ind] != 127 && e_ident[ind] != 'E' &&
+				e_ident[ind] != 'L' && e_ident[ind] != 'F')
+		{
+			dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
+			exit(98);
+		}
+	}
+}
+/**
+* main - Displays ELF Header
+* @c: number of args.
+* @v: pointer to args.
+* Return: 0
+*
+* Description: Prints ELF Header
+*/
+int main(int __attribute__((__unused__)) c, char *v[])
+{
+	int openf, readf;
+	Elf64_Ehdr *header;
+
+	openf = open(v[1], O_RDONLY);
+	if (openf == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", v[1]);
+		exit(98);
+	}
+
+	header = malloc(sizeof(Elf64_Ehdr));
+	if (header == NULL)
+	{
+		close_elf(openf);
+		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", v[1]);
+		exit(98);
+	}
+	readf = read(openf, header, sizeof(Elf64_Ehdr));
+	if (readf == -1)
+	{
+		free(header);
+		close_elf(openf);
+		dprintf(STDERR_FILENO, "Error: `%s`: No such file\n", v[1]);
+		exit(98);
+	}
+
+	elf_checker(header->e_ident);
+	printf("ELF Header:\n");
+	magic_pr(header->e_ident);
+	class_pr(header->e_ident);
+	data_pr(header->e_ident);
+	version_pr(header->e_ident);
+	osabi_pr(header->e_ident);
+	abi_pr(header->e_ident);
+	type_pr(header->e_type, header->e_ident);
+	entry_pr(header->e_entry, header->e_ident);
+
+	free(header);
+	close_elf(openf);
+	return (0);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,9 +6,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+#include <elf.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+
+void elf_checker(unsigned char *e_ident);
+void magic_pr(unsigned char *e_ident);
+void class_pr(unsigned char *e_ident);
+void data_pr(unsigned char *e_ident);
+void version_pr(unsigned char *e_ident);
+void abi_pr(unsigned char *e_ident);
+void osabi_pr(unsigned char *e_ident);
+void type_pr(unsigned int e_type, unsigned char *e_ident);
+void entry_pr(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
 #endif
