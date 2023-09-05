027f97c3d86434e4e98769fcafd4310af5708685
PinkNoteX <omarfarghaly8@gmail.com>
2023-09-04 17:17:41 -0700

    Task 3 Done

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,20 +8,20 @@
 
 int main(int c, char **v)
 {
-	int file1, file2, check;
 	char buffer[1024];
+	int file1, file2, check;
 
-	if (c != 3)
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	file1 = open(v[1], O_RDONLY);
+	file2 = open(v[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (file1 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", v[1]);
 		exit(98);
 	}
-	file2 = open(v[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (file2 == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", v[2]), exit(99);
+	if (c != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	while (0 < (check = read(file1, buffer, 1024)))
 	{
 		if (check == -1)
