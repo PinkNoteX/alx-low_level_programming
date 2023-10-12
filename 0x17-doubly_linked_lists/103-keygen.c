3e6e95e396111c1ac36c3591f61b51531c43e764
PinkNoteX <omarfarghaly8@gmail.com>
2023-10-12 11:02:21 -0700

    ADV 3 file and prepping

---
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - it's supposed to gen a key
+ * @argc: number of args
+ * @argv: array of pointers to args
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	(void) argv;
+	return (0);
+}
