3a7ac1e624193fc73c507a04493f577f2b7b2e0a
PinkNoteX <omarfarghaly8@gmail.com>
2023-10-12 07:15:57 -0700

    Task 1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,9 +1,23 @@
 #include "lists.h"
 /**
  * dlistint_len - returns the number of elements in dlinked list
- *
+ * @h: head of dlistint_t
+ * Return: Number of elements in linked list
  */
 size_t dlistint_len(const dlistint_t *h)
 {
+	dlistint_t *a;
+	unsigned int c = 0;
 
+	a = malloc(sizeof(dlistint_t));
+	a = h;
+
+	if (!a)
+		return (0);
+	while (a)
+	{
+		a = a->next;
+		c++;
+	}
+	return (c);
 }
