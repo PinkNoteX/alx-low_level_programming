97cf15c6aaa1fbb12d68572b8bb7ef6a0d7907c8
PinkNoteX <omarfarghaly8@gmail.com>
2024-02-06 14:40:48 -0800

    Task 1

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,4 +3,6 @@
 #include <stdlib.h>
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+void printarr(int *arr, int start, int end);
+int divide(int *arr, int target, int start, int end);
 #endif
